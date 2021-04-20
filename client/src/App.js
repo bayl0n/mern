0489f37521495a8db327b1cb61a107d595614d45
Nathan <nathanbaylon@gmail.com>
2021-04-20 23:47:52 +1000

    Implemented redux

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,17 @@ import './App.css';
 import AppNavBar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 function App() {
   return (
-    <div className="App">
-      <AppNavBar/>
-      <ShoppingList/>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <AppNavBar/>
+        <ShoppingList/>
+      </div>
+    </Provider>
   );
 }
 
